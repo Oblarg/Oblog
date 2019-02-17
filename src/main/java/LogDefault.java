997d4d2e604b7a902a0514e06d7306cf645a19d1
Eli <emichaelbarnett@gmail.com>
2019-02-17 17:14:32 -0500

    formatting

--- src/main/java/LogDefault.java
+++ src/main/java/LogDefault.java
@@ -2,11 +2,12 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Inherited
 /**
  * Displays a value as its default widget type on Shuffleboard.  Provides no configuration options.
  */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface LogDefault {
     //The name of the value on Shuffleboard; defaults to field or method name.
     String name() default "NO_NAME";
