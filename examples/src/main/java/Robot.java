1129fd546509975e865d1a952d64b691debb5a53
Eli <emichaelbarnett@gmail.com>
2019-02-21 18:54:34 -0500

    fix example autonomous

--- examples/src/main/java/Robot.java
+++ examples/src/main/java/Robot.java
@@ -78,13 +78,7 @@ public class Robot extends TimedRobot {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
-  }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
     switch (m_autoSelected) {
       case kCustomAuto:
         command5Seconds.start();
@@ -94,6 +88,13 @@ public class Robot extends TimedRobot {
         command10Seconds.start();
         break;
     }
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
     Logger.updateEntries();
   }
 
