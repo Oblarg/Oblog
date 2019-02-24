07da6269de330c9ee1106a9dbb3e1c507cff097a
Eli <emichaelbarnett@gmail.com>
2019-02-23 22:35:27 -0500

    update documentation

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.github.oblarg.logexample"},{"l":"io.github.oblarg.oblog"},{"l":"io.github.oblarg.oblog.annotations"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.github.oblarg.oblog"},{"l":"io.github.oblarg.oblog.annotations"}]
