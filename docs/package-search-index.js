4d6ce1959ded801e854c1465e0b42ec5bbda1637
Eli <emichaelbarnett@gmail.com>
2019-02-23 22:17:09 -0500

    update documentation

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.github.oblarg.logexample"},{"l":"io.github.oblarg.oblog"},{"l":"io.github.oblarg.oblog.annotations"}]
