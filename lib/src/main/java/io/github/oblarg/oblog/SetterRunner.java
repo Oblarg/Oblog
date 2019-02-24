ab9a79304e602b532a4bc711d1bf1a6e884eddc0
Eli <emichaelbarnett@gmail.com>
2019-02-24 11:29:35 -0500

    begin adding config support

---
+++ lib/src/main/java/io/github/oblarg/oblog/SetterRunner.java
@@ -0,0 +1,22 @@
+package io.github.oblarg.oblog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+class SetterRunner implements Executor {
+
+    private List<Runnable> setters = new ArrayList<>();
+
+    @Override
+    public synchronized void execute(Runnable command) {
+        setters.add(command);
+    }
+
+    synchronized void runSynchronous() {
+        for(Runnable setter: setters) {
+            setter.run();
+        }
+        setters.clear();
+    }
+}
