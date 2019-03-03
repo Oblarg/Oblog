b3bcb746b8cfe7722c898eec3b71b247f131fdd0
Eli <emichaelbarnett@gmail.com>
2019-03-03 16:30:41 -0500

    make ShuffleboardContainerWrapper public

--- lib/src/main/java/io/github/oblarg/oblog/ShuffleboardContainerWrapper.java
+++ lib/src/main/java/io/github/oblarg/oblog/ShuffleboardContainerWrapper.java
@@ -3,7 +3,12 @@ package io.github.oblarg.oblog;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.shuffleboard.LayoutType;
 
-interface ShuffleboardContainerWrapper {
+/**
+ * A wrapper for the WPILib ShuffleboardContainer class; wrapped to allow the Logger to substitute a NT-only implementation
+ * if desired.
+ */
+
+public interface ShuffleboardContainerWrapper {
     ShuffleboardLayoutWrapper getLayout(String title, LayoutType type);
 
     SimpleWidgetWrapper add(String title, Object defaultValue);
