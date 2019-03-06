44f97f9c12aa05e81a4d817fc1787381029efaa5
Eli <emichaelbarnett@gmail.com>
2019-03-06 12:36:25 -0500

    improve recording filler util

--- util/ShuffleboardLogCompleter.R
+++ util/ShuffleboardLogCompleter.R
@@ -7,10 +7,21 @@ fillShuffleboardRecording <- function(dat) {
                            colnames(dat) != "Event.Description" || 
                            colnames(dat) != "Event.Severity"]) {
     for(i in 2:nrow(dat)) {
-      if(is.na(dat[i, col])) {
+      if(is.na(dat[i, col]) || dat[i, col] == "") {
         dat[i, col] <- dat[i-1, col]
       }
     }
   }
   return(dat)
 }
+
+
+fillFile <- function(file) {
+  dat <- read.csv(file)
+  dat <- fillShuffleboardRecording(dat)
+  write.csv(dat, file = file)
+}
+
+
+files <- list.files(pattern = "*.csv")
+lapply(files, fillFile)
