a60f05ec6e8d05609633f190209881c2d27cd24a
Eli <emichaelbarnett@gmail.com>
2019-03-06 18:20:02 -0500

    update util script

--- util/ShuffleboardLogCompleter.R
+++ util/ShuffleboardLogCompleter.R
@@ -1,6 +1,15 @@
 #Simple utility for filling in missing data from Shuffleboard recording files.
 #Shuffleboard recordings contain missing values when values are not changing, this function fills them in.
-#Shuffleboard recordings should probably be converted to CSV before being loaded into R.
+#Shuffleboard recordings must be converted to .CSV for this tool to work on them.
+
+#To use this script, ensure that R is installed on your machine and has been added to the PATH.
+#Then, move this script and the associated .bat file into the directory of the shuffleboard recordings.
+#Finally, run the .bat file.  If nothing happens, run it as an administrator (Windows sucks).
+
+#Warning: This script will fill *all* .csv files in the directory.  Be sure there are no other
+#.csv files located in the directory that you do not want modified.
+
+args<-commandArgs(trailingOnly = TRUE)
 
 fillShuffleboardRecording <- function(dat) {
   for(col in colnames(dat)[colnames(dat) != "Event" || 
@@ -23,5 +32,5 @@ fillFile <- function(file) {
 }
 
 
-files <- list.files(pattern = "*.csv")
+files <- list.files(path = as.character(args[1]),pattern = "*.csv")
 lapply(files, fillFile)
